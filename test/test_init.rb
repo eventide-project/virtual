46686eeb5f4f5f62f31b4c912bd685a4f4c6f12f
Scott Bellware <sbellware@gmail.com>
2015-11-11 00:50:57 -0600

    Init files are setup to work without bundler

--- test/test_init.rb
+++ test/test_init.rb
@@ -1,10 +1,11 @@
 ENV['CONSOLE_DEVICE'] ||= 'stdout'
-ENV['CONSOLE_COLOR'] ||= 'on'
+ENV['LOG_COLOR'] ||= 'on'
 ENV['LOG_LEVEL'] ||= 'trace'
 
 puts RUBY_DESCRIPTION
 
 require_relative '../init.rb'
+
 require 'runner'
 
-TestLogger = Telemetry::Logger.get 'Test Output'
+Telemetry::Logger::AdHoc.activate
