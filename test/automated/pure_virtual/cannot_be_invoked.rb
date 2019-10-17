4b2ae8fade2c98216265ecf3631627cc3a30e2b9
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-17 16:35:13 -0500

    Parenthesis are added to assert_raises and refute_raises

--- test/automated/pure_virtual/cannot_be_invoked.rb
+++ test/automated/pure_virtual/cannot_be_invoked.rb
@@ -5,7 +5,7 @@ context "Pure Virtual" do
     example = Controls::PureVirtualMethod.example
 
     test "Raises an error" do
-      assert_raises Virtual::PureMethod::Error do
+      assert_raises(Virtual::PureMethod::Error) do
         example.some_pure_virtual_method
       end
     end
--- test/automated/pure_virtual/implemented_in_subclass.rb
+++ test/automated/pure_virtual/implemented_in_subclass.rb
@@ -5,7 +5,7 @@ context "Pure Virtual" do
     example = Controls::PureVirtualMethod::Implemented.example
 
     test "Does not raise an error" do
-      refute_raises Virtual::PureMethod::Error do
+      refute_raises(Virtual::PureMethod::Error) do
         example.some_pure_virtual_method
       end
     end
