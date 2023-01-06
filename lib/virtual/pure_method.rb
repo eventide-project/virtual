8ee9abe9085635ced02fad8ac4f02fe242963de0
Aaron Jensen <aaronjensen@gmail.com>
2023-01-06 09:50:00 -0500

    Pure method error message class is corrected

--- lib/virtual/pure_method.rb
+++ lib/virtual/pure_method.rb
@@ -8,7 +8,7 @@ module Virtual
 
     def self.define(target_class, method_name)
       target_class.send :define_method, method_name do |*args|
-        raise Error, "\"#{method_name}\" of #{target_class.name}"
+        raise Error, "\"#{method_name}\" of #{self.class.name}"
       end
     end
   end
