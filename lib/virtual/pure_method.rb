744cec4c20a8235c0291ea0893a68b7f5d52fda6
Scott Bellware <sbellware@gmail.com>
2023-01-10 09:48:52 -0600

    Method existence check removed as it caused regressions

--- lib/virtual/pure_method.rb
+++ lib/virtual/pure_method.rb
@@ -3,9 +3,10 @@ module Virtual
     Error = Class.new(RuntimeError)
 
     def self.define(target_class, method_name)
-      if target_class.method_defined?(method_name)
-        raise Error, "#{target_class} already has an implementation of the #{method_name} method"
-      end
+      ## Re-enable when possible. Caused regressions in core libraries - Scott, Tue Jan 10 2022
+      # if target_class.method_defined?(method_name)
+      #   raise Error, "#{target_class} already has an implementation of the #{method_name} method"
+      # end
 
       target_class.send(:define_method, method_name) do |*args|
         raise Error, "Pure virtual (abstract) method #{method_name} of #{self.class.name} must be implemented"
--- lib/virtual/virtual_method.rb
+++ lib/virtual/virtual_method.rb
@@ -3,9 +3,10 @@ module Virtual
     Error = Class.new(RuntimeError)
 
     def self.define(target_class, method_name, &blk)
-      if target_class.method_defined?(method_name)
-        raise Error, "#{target_class} already has an implementation of the #{method_name} method"
-      end
+      ## Re-enable when possible. Caused regressions in core libraries - Scott, Tue Jan 10 2022
+      # if target_class.method_defined?(method_name)
+      #   raise Error, "#{target_class} already has an implementation of the #{method_name} method"
+      # end
 
       blk ||= proc do |*|
       end
